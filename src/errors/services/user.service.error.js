82f4893232ccf2e38e922f2df1e706b024c69a7b
jackson-elfers <jacksonelfersdev@outlook.com>
2020-07-28 14:32:56 -0700

    refactor: move user settings to services

--- src/errors/services/user.service.error.js
+++ src/errors/services/user.service.error.js
@@ -3,17 +3,67 @@ module.exports = class {
     this.method = props.method;
   }
 
+  // helpers
+
+  async _validEmail(data) {
+    this.method.check.assert(this.method.check.object(data), "expected object as first argument");
+    if (!this.method.emailValidator.validate(data.email)) {
+      throw new Error("email must be a valid email address.");
+    }
+  }
+
+  // errors
+
   async login(data) {}
 
-  async register(data) {}
+  async register(data) {
+    this.method.check.assert(this.method.check.object(data), "expected object as first argument");
+
+    const email = this.method.config.settings.user.email;
+    const password = this.method.config.settings.user.password;
+    await this._validEmail(data);
+    if (data.email.length < email.min) {
+      throw new Error(`email must be greater than ${email.min} characters.`);
+    }
+    if (data.email.length > email.max) {
+      throw new Error(`email must be less than ${email.max} characters.`);
+    }
+    if (data.password.length < password.min) {
+      throw new Error(`Password must be greater than ${password.min} characters.`);
+    }
+    if (data.password.length > password.max) {
+      throw new Error(`Password must be less than ${password.max} characters.`);
+    }
+  }
 
   async readSingleId(data) {}
 
   async emailExists(data) {}
 
-  async updateEmail(data) {}
+  async updateEmail(data) {
+    this.method.check.assert(this.method.check.object(data), "expected object as first argument");
 
-  async updatePassword(data) {}
+    const email = this.method.config.settings.user.email;
+    await this._validEmail(data);
+    if (data.email.length < email.min) {
+      throw new Error(`email must be greater than ${email.min} characters.`);
+    }
+    if (data.email.length > email.max) {
+      throw new Error(`email must be less than ${email.max} characters.`);
+    }
+  }
+
+  async updatePassword(data) {
+    this.method.check.assert(this.method.check.object(data), "expected object as first argument");
+
+    const password = this.method.config.settings.user.password;
+    if (data.password.length < password.min) {
+      throw new Error(`Password must be greater than ${password.min} characters.`);
+    }
+    if (data.password.length > password.max) {
+      throw new Error(`Password must be less than ${password.max} characters.`);
+    }
+  }
 
   async unregister(data) {}
 };
