f63ac62b9ad41f9c6875020f614338aaf7514409
jackson-elfers <jacksonelfersdev@outlook.com>
2020-09-08 15:30:37 -0700

    add: homepage

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -7,21 +7,25 @@ function Main(props) {
     <div>
       <hr />
       <div className="box">
-        <p>Hi Betty,</p>
+        <h2>Standing in Solidarity with Black Lives Matter</h2>
+
         <p>
-          So the site is nearly complete, still need to build a persuasive homepage, admin controls, and upload
-          functionality for user profiles. Take a look around and let me know of anything you'd like to add. You can
-          call/text/email whenever you'd like. You have a bunch of fake users for demo purposes search for "software
-          engineer" and you'll hit about 30 of them.
-        </p>
-        <p>
-          Does this style/color work for you? We can change everything if you'd like. I'm looking forward to building
-          this into android/ios apps along with maybe pwa so we'll have maximum coverage on all platforms. I might hire
-          a copywriter to help with the actual site design I know a good one LinkedIn. Anyhow, things are going well,
-          test drive it and let me know!
+          Ebenee is devoted to helping connect recruiters to job seekers among the black and minority communities around
+          the world to help lift those out of poverty to help repair fractured lives and restore lost communities.
         </p>
       </div>
+
       <img src={`${process.env.REACT_APP_API}/images/bonfire.gif`} />
+
+      <div className="box">
+        <h2>Join our community and connect with companies looking to help bring job seekers back into society.</h2>
+
+        <p>Create a profile, upload a resume and talk to recruiters looking to hire talent just like you.</p>
+      </div>
+
+      <Link to={`${routes.Register}`}>
+        <button>Get Started</button>
+      </Link>
     </div>
   );
 }
